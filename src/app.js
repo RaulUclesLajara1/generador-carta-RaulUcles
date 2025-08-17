4b1c74b79cbd5bfe41bbe237daa8acb1b2e8d2bc
Raúl Uclés Lajara <rul2@alu.ua.es>
2025-08-17 17:43:06 +0200

    proyecto terminado con boton e inputs(a falta de temporizador)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
-import "bootstrap";
-import "./style.css";
+
+function Funcion() {
+    
+    const numeros = ['1','2','3','4','5','6','7','8','9','10','J','Q','K','A']
+    const palos = ['♦','♥','♠','♣']
+
+    let icono1_doc = document.getElementById('icono1')
+    let icono2_doc = document.getElementById('icono2')
+    let numero_doc = document.getElementById('numero')
+
+    let numero = numeros[Math.floor(Math.random()*numeros.length)]
+    let palo = palos[Math.floor(Math.random()*palos.length)]
+
+    icono1_doc.innerHTML = palo
+    icono2_doc.innerHTML = palo
+    numero_doc.innerHTML = numero
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+    if(palo == '♦'|| palo=='♥'){
+        icono1_doc.style.color = 'red'
+        icono2_doc.style.color = 'red'
+        
+    
+    }else{
+        icono1_doc.style.color = 'black'
+        icono2_doc.style.color = 'black'
+
+        
+    }
+
+
+
+
 };
+let ancho = document.getElementById("ancho")
+let alto = document.getElementById("alto")
+let carta = document.getElementById("carta-id")
+
+ancho.addEventListener("input", () => {
+    carta.style.width = `${ancho.value}px`;
+  });
+
+alto.addEventListener("input", () => {
+    carta.style.height = `${alto.value}px`;
+  });
+
+document.getElementById("boton").addEventListener("click", Funcion);
